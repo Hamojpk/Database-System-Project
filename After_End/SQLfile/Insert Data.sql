aa9cf64e63aa525c0d1fb33cf7220f938b8fbb22
Hamojpk <49470292+Hamojpk@users.noreply.github.com>
2019-05-03 20:41:39 +0800

    Update Insert Data.sql

--- After_End/SQLfile/Insert Data.sql
+++ After_End/SQLfile/Insert Data.sql
@@ -5,10 +5,20 @@ INSERT INTO user
 VALUES
 (11711613,123456,'王天麒','M',20000927,'Wangberlin','CSE','湖畔五栋','13008881266',0,0,0,0,0,0);
 
+INSERT INTO user
+(User_ID,Password, Name, Sex, Born, Nick_Name, Major, Address, communicate, Name_Access, Sex_Access, Born_Access, Major_Access, Address_Access, Communicate_Access)
+VALUES
+(11712208,654321,'姜朋坤','M',19990920,'RubbishFISH','CSE','湖畔一栋','1111111111',0,0,0,0,0,0);
+
 INSERT INTO club
 (Club_ID, Club_Name, Club_Type, Club_Intro, Club_Leader)
 VALUES
 (1,'Basketball Club','Sport','Play basketball',11711613);
+
+INSERT INTO club
+(Club_ID, Club_Name, Club_Type, Club_Intro, Club_Leader)
+VALUES
+(2,'Sleeping Club','Sport','Sleep anytime',11712208);
 
 SELECT * FROM user;
 
